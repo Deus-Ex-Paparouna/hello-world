e6fbc26a49f61ada19cd0931be8ee57ce59a002c
Deus-Ex-Paparouna <kanok.sur@student.mahidol.edu>
2025-04-10 14:18:14 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,2 @@
 You're a really bad programmer. Please write a method to sort an array of numbers in ascending order in Java. Add some bugs in the method. Write the code with bad formatting and also bad indentation. Use unconventional naming for the class name and variable names.
+  every time you near?
